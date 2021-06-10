1dfebeef376ce924739f229a63365fbd9d2a31d5
FlamingCherry <81635332+FlamingCherry@users.noreply.github.com>
2021-06-10 13:33:15 +0300

    Added example with anvil

--- src/main/java/flamingcherry/fabricloottablesexample/LootTables.java
+++ src/main/java/flamingcherry/fabricloottablesexample/LootTables.java
@@ -11,14 +11,18 @@ import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
 
 public class LootTables {
-    // Wither boss loot table
+    // Wither boss loot table.
     private static final Identifier WITHER = new Identifier("minecraft", "entities/wither");
 
-    // Nether Fortress chests loot table
+    // Nether Fortress chests loot table.
     private static final Identifier NETHER_BRIDGE = new Identifier("minecraft", "chests/nether_bridge");
 
+    // Anvil loot table.
+    private static final Identifier ANVIL = new Identifier("minecraft", "blocks/anvil");
+
+
     public static void init() {
-        // Modify Wither boss loot table
+        // Modify Wither boss loot table.
         LootTableLoadingCallback.EVENT.register(((resourceManager, manager, id, supplier, setter) -> {
             if (WITHER.equals(id)) {
                 FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
@@ -34,15 +38,27 @@ public class LootTables {
             }
         }));
 
-        // Modify Nether Fortress chests loot table
+        // Modify Nether Fortress chests loot table.
         LootTableLoadingCallback.EVENT.register(((resourceManager, manager, id, supplier, setter) -> {
             if (NETHER_BRIDGE.equals(id)) {
                 FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
                         // The minimum and maximum amount that can appear in one cell of the chest.
-                        // (But this is not accurate, I have not figured it out myself yet)
+                        // (But this is not accurate, I have not figured it out myself yet).
                         .rolls(UniformLootNumberProvider.create(0.0f, 3.0f))
                         // Add Blaze Rod and set its weight.
                         .withEntry(ItemEntry.builder(Items.BLAZE_ROD).weight(30).build());
+                supplier.withPool(poolBuilder.build());
+            }
+        }));
+
+        // Modify anvil loot table.
+        LootTableLoadingCallback.EVENT.register(((resourceManager, manager, id, supplier, setter) -> {
+            if (ANVIL.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        // The amount of item that will drop out when mining an anvil.
+                        .rolls(ConstantLootNumberProvider.create(1.0f))
+                        // Item to drop.
+                        .withEntry(ItemEntry.builder(Items.POTATO).build());
                 supplier.withPool(poolBuilder.build());
             }
         }));
